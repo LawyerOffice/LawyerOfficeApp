bcfb0137eec3e88b30d597a7ed7488f147dda8b4
ivan16luis <ivanlugiz16@yahoo.com>
2016-03-07 17:14:02 -0500

    comit: asig proceso

---
+++ src/java/com/beans/AdminCasosBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Ivan
+ */
+@ManagedBean
+@ViewScoped
+public class AdminCasosBean {
+
+    /**
+     * Creates a new instance of AdminCasosBean
+     */
+    public AdminCasosBean() {
+    }
+    
+
+    
+}
