dd968d13ae5ad9ac1ea81bb408f48e49b96e7157
ivan16luis <ivanlugiz16@yahoo.com>
2016-03-18 12:48:24 -0500

    Luis: seguridad logueo

--- src/java/com/web/CacheControlPhaseListener.java
+++ src/java/com/web/CacheControlPhaseListener.java
@@ -28,13 +28,12 @@ public class CacheControlPhaseListener implements PhaseListener {
         FacesContext facesContext = pe.getFacesContext();
         HttpServletResponse response = (HttpServletResponse) facesContext
                 .getExternalContext().getResponse();
-        response.addHeader("RapidLoansDemo_v1_1", "no-cache");
+        response.addHeader("LawyerOfficeApp", "no-cache");
         response.addHeader("Cache-Control", "no-cache");
         // Stronger according to blog comment below that references HTTP spec
         response.addHeader("Cache-Control", "no-store");
         response.addHeader("Cache-Control", "must-revalidate");
         // some date in the past
-        response.addHeader("Expires", "Mon, 8 Aug 2006 10:00:00 GMT");
     }
 
     @Override
